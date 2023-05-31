140766c12d01411524da421645109f4f3b2a1b16
PREM KUMAR <premenaga@gmail.com>
2023-05-31 15:39:20 +0530

    removed leaflet

--- src/components/CWData.js
+++ src/components/CWData.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useQuery, QueryClient, QueryClientProvider } from 'react-query';
 import axios from 'axios';
 import { Line } from 'react-chartjs-2';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const queryClient = new QueryClient();
 
@@ -27,26 +26,6 @@ const LineGraph = ({ data }) => {
     );
 };
 
-const MapMarkers = ({ data }) => {
-    return (
-        <MapContainer center={[0, 0]} zoom={2} style={{ height: '400px', width: '100%' }}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            {data.map((country) => (
-                <Marker key={country.countryInfo.iso3} position={[country.countryInfo.lat, country.countryInfo.long]}>
-                    <Popup>
-                        <div>
-                            <h4>{country.country}</h4>
-                            <p>Total Cases: {country.cases}</p>
-                            <p>Total Deaths: {country.deaths}</p>
-                            <p>Total Recovered: {country.recovered}</p>
-                        </div>
-                    </Popup>
-                </Marker>
-            ))}
-        </MapContainer>
-    );
-};
-
 const CountryData = () => {
     const fetchCountryData = async () => {
         const response = await axios.get('https://disease.sh/v3/covid-19/countries');
@@ -64,11 +43,10 @@ const CountryData = () => {
     }
 
     return (
-        <div className="w-full p-4 h-screen overflow-scroll text-gray-200">
+        <div className="w-full p-4 h-screen  text-gray-200">
             <div className="bg-gray-800 rounded-lg shadow-md p-6">
-                <h3 className="text-xl  font-semibold mb-4">COVID-19 Cases by Country</h3>
+                <h3 className="text-xl font-semibold mb-4">COVID-19 Cases by Country</h3>
                 <LineGraph data={data} />
-                <MapMarkers data={data} />
             </div>
         </div>
     );
